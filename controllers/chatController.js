7d5c2d4997b92a778e5ab07f851e3c81e98ac0f0
Ash Magill <ash@Ashs-MBP.fritz.box>
2022-08-07 18:40:31 +1200

    goals controller and routes setup plus error handler

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,10 +1,26 @@
+const asyncHandler = require("express-async-handler");
 const dummyUsers = require("../utils/dummyData");
-const getChat = (req, res) => {
-  try {
-    res.json(dummyUsers);
-  } catch (error) {
-    console.log(error.message);
-  }
-};
 
-module.exports = { getChat };
+//instead of try catch, I'm wrapping everything in express-async-handler
+
+const getMessages = asyncHandler(async (req, res) => {
+  res.json(dummyUsers);
+});
+
+const createMessage = asyncHandler(async (req, res) => {
+  if (!req.body.message) {
+    res.status(400);
+    throw new Error("Please provide a text");
+  }
+  res.json({ message: "createMessage" });
+});
+
+const editMessage = asyncHandler(async (req, res) => {
+  res.json({ message: `update goal ${req.params.id}` });
+});
+
+const deleteMessage = asyncHandler(async (req, res) => {
+  res.json({ message: `delete goal ${req.params.id}` });
+});
+
+module.exports = { getMessages, createMessage, editMessage, deleteMessage };
